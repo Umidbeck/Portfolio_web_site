a19b50c251c17b6a7095fcdee12605b1b3beb85c
Umidjon <79796851+Umidbeck@users.noreply.github.com>
2024-10-08 17:47:41 +0500

    Add files via upload

--- config/settings.py
+++ config/settings.py
@@ -28,9 +28,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env.str("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = env.bool("DEBUG", default = False)
+DEBUG = True
 
-ALLOWED_HOSTS = ['my-django-app.onrender.com', '127.0.0.1']
+ALLOWED_HOSTS = ['.onrender.com', '127.0.0.1']
 
 
 # Application definition
